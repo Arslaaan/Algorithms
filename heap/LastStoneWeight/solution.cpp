23fdce9cf30226844eb84090f2617a7660481f37
Arslaaan <aochirov@gmail.com>
2022-10-24 16:43:37 +0300

    +1 task

--- heap/LastStoneWeight/solution.cpp
+++ heap/LastStoneWeight/solution.cpp
@@ -14,6 +14,23 @@ class Solution {
 
 public:
     int lastStoneWeight(vector<int> &stones) {
+        priority_queue<int, vector<int>, less<>> priorityQueue(stones.begin(), stones.end());
 
+        while (priorityQueue.size() > 1) {
+            int x = priorityQueue.top();
+            priorityQueue.pop();
+            int y = priorityQueue.top();
+            priorityQueue.pop();
+
+            if (x == y) {
+                continue;
+            } else {
+                priorityQueue.push(abs(x - y));
+            }
+        }
+        if (priorityQueue.empty()) {
+            return 0;
+        }
+        return priorityQueue.top();
     }
 };
