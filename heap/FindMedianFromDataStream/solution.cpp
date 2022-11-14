ac27d7d20b67cf2aab22a0b28569a5df6e342ad7
Arslaaan <aochirov@gmail.com>
2022-11-14 18:08:50 +0300

    +1 task

--- heap/FindMedianFromDataStream/solution.cpp
+++ heap/FindMedianFromDataStream/solution.cpp
@@ -12,8 +12,8 @@
 using namespace std;
 
 class MedianFinder {
-    priority_queue<int, vector<int>, less<>> maxHeap;
-    priority_queue<int, vector<int>, greater<>> minHeap;
+    priority_queue<int, vector<int>, less<>> maxHeap; // left part
+    priority_queue<int, vector<int>, greater<>> minHeap; // right part
 public:
     MedianFinder() {
     }
@@ -28,15 +28,36 @@ public:
     }
 
     void balanceHeaps() {
-        // если разница между кучами строго больше 1, то их нужно балансировать
-        // но надо не забыть, правильно посчитать разницу тк это unsigned числа
-        bool isLeftHeapGreater = max(minHeap.size(), maxHeap.size()) == maxHeap.size();
-
-        if (isLeftHeapGreater) {
-            // pop from left to right
+        if (maxHeap.size() != minHeap.size()) {
+            // если разница между кучами строго больше 1, то их нужно балансировать
+            // но надо не забыть, правильно посчитать разницу тк это unsigned числа
+            bool isLeftHeapGreater = max(minHeap.size(), maxHeap.size()) == maxHeap.size();
+            size_t diffSize = isLeftHeapGreater ? maxHeap.size() - minHeap.size() : minHeap.size() - maxHeap.size();
+            if (diffSize > 1) {
+                if (isLeftHeapGreater) {
+                    // pop from left to right
+                    int top = maxHeap.top();
+                    maxHeap.pop();
+                    minHeap.push(top);
+                } else {
+                    // pop from right to left
+                    int top = minHeap.top();
+                    minHeap.pop();
+                    maxHeap.push(top);
+                }
+            }
         }
     }
 
     double findMedian() {
+        if (maxHeap.size() == minHeap.size() && !maxHeap.empty()) {
+            return (maxHeap.top() + minHeap.top()) / 2.0;
+        } else {
+            if (maxHeap.size() > minHeap.size()) {
+                return maxHeap.top();
+            } else {
+                return minHeap.top();
+            }
+        }
     }
 };
